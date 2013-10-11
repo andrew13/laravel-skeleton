5539b5ca1e9d97f37bbf8b39dbd4ffda92bd76e5
Eugene Lee <eugene.lee@hyfn.com>
2013-10-11 23:43:41 +0000

    Login action for user

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,8 +1,30 @@
 <?php
 
 class AuthController extends BaseController {
+	/**
+	* Log the user in
+	*/
+	public function login()
+	{
+		try {
+			// Set login credentials
+			$credentials = array(
+				'username' => Input::get('username'), 'password' => Input::get('password')
+			);
 
-
-
-
+			// Try to authenticate the user
+			$user = Sentry::authenticate($credentials, false);
+			if ($user) {
+				return Redirect::to('/');
+			}
+		} catch (Cartalyst\Sentry\Users\LoginRequiredException $e) {
+			return Redirect::to('login')->withErrors('Username field is required');
+		} catch (Cartalyst\Sentry\Users\PasswordRequiredException $e) {
+			return Redirect::to('login')->withErrors('Password cannot be blank');
+		} catch (Cartalyst\Sentry\Users\UserNotFoundException $e) {
+			return Redirect::to('login')->withErrors('Could not find user with those credentials');
+		} catch (Cartalyst\Sentry\Users\WrongPasswordException $e) {
+			return Redirect::to('login')->withErrors('The credentials you provided were incorrect');
+		}
+	}
 }
