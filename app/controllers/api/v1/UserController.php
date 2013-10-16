e88db9a72577e0b0f168a9ece7c4d55419a47a86
William Wallace <billy.wallace.jr@gmail.com>
2013-10-16 13:38:44 -0700

    API Auth

--- app/controllers/api/v1/UserController.php
+++ app/controllers/api/v1/UserController.php
@@ -1,15 +1,52 @@
-<?php namespace api\v1;
+<?php
 
-use ApiController;
-use User;
-use Api;
 
-class UserController extends ApiController {
+class ApiV1UserController extends ApiController {
 	public function index()
 		{
 			$users =  User::all();
 			return Api::response($users->toArray());
 		}
+
+		public function login()
+		{
+			$input = array(
+				'email'    => Input::get( 'email' ), // May be the username too
+				'username' => Input::get( 'email' ), // so we have to pass both
+				'password' => Input::get( 'password' )
+			);
+
+			
+
+			// If you wish to only allow login from confirmed users, call logAttempt
+			// with the second parameter as true.
+			// logAttempt will check if the 'email' perhaps is the username.
+			if ( Confide::logAttempt( $input, Config::get('confide::login_confirmed') ) )
+			{
+				return Api::response(Confide::user()->toArray());
+			}
+			else
+			{
+				$user = new User;
+
+				// Check if there was too many login attempts
+				if( Confide::isThrottled( $input ) )
+				{
+					$err_msg = Lang::get('confide::confide.alerts.too_many_attempts');
+				}
+				elseif( $user->checkUserExists( $input ) and ! $user->isConfirmed( $input ) )
+				{
+					$err_msg = Lang::get('confide::confide.alerts.not_confirmed');
+				}
+				else
+				{
+					$err_msg = Lang::get('confide::confide.alerts.wrong_credentials');
+				}
+
+				return Api::error( $err_msg );
+			}
+		}
+
 
 		public function create()
 		{
