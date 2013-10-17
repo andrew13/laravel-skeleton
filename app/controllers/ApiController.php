bc9ae219606a468b926ed64ac97d123109dc9e5a
William Wallace <billy.wallace.jr@gmail.com>
2013-10-17 13:33:09 -0700

    Added token to base controller

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -3,4 +3,11 @@
 
 class ApiController extends Controller {
 
+	protected $token;
+
+	public function __construct()
+	{
+		$this->token = Input::get('token');
+	}
+
 }
--- app/controllers/api/v1/UserController.php
+++ app/controllers/api/v1/UserController.php
@@ -3,73 +3,80 @@
 
 class ApiV1UserController extends ApiController {
 
-		public function index()
-		{
-			$users = User::all();
-			return Api::response($users->toArray());
-		}
+	public function index()
+	{
+		$users = User::all();
+		return Api::response($users->toArray());
+	}
 
-		public function create()
-		{
+	public function create()
+	{
 
-		}
+	}
 
-		public function store()
-		{
+	public function store()
+	{
 
-		}
+	}
 
-		public function show($user_id)
-		{
+	public function show($user_id)
+	{
 
-		}
+	}
 
-		public function edit($user_id)
-		{
+	public function edit($user_id)
+	{
 
-		}
+	}
 
-		public function update($user_id)
-		{
+	public function update($user_id)
+	{
 
-		}
+	}
 
-		public function destroy($user_id)
-		{
+	public function destroy($user_id)
+	{
 
-		}
+	}
 
-		public function login()
-		{
-			$rules = [
-				'email' => 'required_without:username|email',
-				'username' => 'required_without:email',
-				'password' => 'required'
-			];
+	/**
+	 * Api Login
+	 * @return array
+	 */
+	public function login()
+	{
+		$rules = [
+			'email' => 'required_without:username|email',
+			'username' => 'required_without:email',
+			'password' => 'required'
+		];
 
-			$validate = Hyfn::validate($rules);
-			if($validate!==true) return Api::error($validate);
+		$validate = Hyfn::validate($rules);
+		if($validate!==true) return Api::error($validate);
 
-			$input = array(
-				'email'    => Input::get( 'email' ), // May be the username too
-				'username' => Input::get( 'username' ),
-				'password' => Input::get( 'password' )
-			);
+		$input = array(
+			'email'    => Input::get( 'email' ), // May be the username too
+			'username' => Input::get( 'username' ),
+			'password' => Input::get( 'password' )
+		);
 
-			$user = new User;
-			$login = $user->login($input);
-			if($login !== true) return Api::error($login);
+		$user = new User;
+		$login = $user->login($input);
+		if($login !== true) return Api::error($login);
 
-			$confideUser = Confide::user();
-			$confideUser->token = User::getToken($confideUser->id);
+		$confideUser = Confide::user();
+		$confideUser->token = User::getToken($confideUser->id);
 
-			return Api::response($confideUser->toArray());
-		}
+		return Api::response($confideUser->toArray());
+	}
 
-		public function logout()
-		{
-			$token = Input::get('token');
-			User::invalidateToken($token);
-			return Api::response(['success' => true]);
-		}
+	/**
+	 * Remove auth token from cache
+	 * @return array
+	 */
+	public function logout()
+	{
+		User::invalidateToken($this->token);
+		return Api::response(['success' => true]);
+	}
 }
